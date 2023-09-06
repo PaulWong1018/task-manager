dbcdb7f2a93466f24a6dcb8380f2bc47ca18618d
Paul Wong <shuipowong@gmail.com>
2023-09-06 15:14:45 -0400

    install tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,73 @@
-import './style.css'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import './style.css';
 
-function App() {
-
-  return (
-    <>
-    Hello
-    </>
-  )
+interface TaskModel {
+  id: number;
+  title: string;
+  dueDate: Date;
+  category: string;
 }
 
-export default App
+import React, { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+
+  const handleInputChange = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setTask(e.target.value);
+  };
+
+  function handleAddTodo(): void {
+    if (task.trim() === "") return; // Don't add empty tasks
+
+    setTodos([...todos, task]);
+    setTask("");
+  }
+
+  const handleRemoveTodo = (index: number) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1 className='text-3xl py-5'>Task Management App</h1>
+      <form className='space-y-5'>
+        <h3>Title</h3>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={task}
+          onChange={handleInputChange}
+        />
+        <h3>Due Date</h3>
+        <input type="date" placeholder="dd/mm/yyyy" />
+
+        <h3>Category</h3>
+        <select name="cars" id="cars">
+          <option value="volvo">Groceries</option>
+          <option value="saab">Utilities</option>
+          <option value="opel">Entertainment</option>
+        </select>
+
+        <button className="block" onClick={handleAddTodo}>
+          Submit
+        </button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleRemoveTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
